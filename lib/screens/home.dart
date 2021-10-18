8249eb57b4608f0cbd168d1016b955556ec54565
Willyanllopes <willyanlopes3@gmail.com>
2021-10-18 02:52:54 -0300

    Primeiro Commit da Aplicação de clima

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dropdown_search/dropdown_search.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,7 +9,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final List<String> _cidade = [
+  final List<String> _cidades = [
 
     "Aracaju",
     "Belém",
@@ -39,8 +40,73 @@ class _HomeState extends State<Home> {
     "Vitória",
 
   ];
+  String _cidadeSelecionada ="São Paulo";
+  carregaCLima () async {
+
+    const String  _appid = "c92c5b87fc513766c06716a7b1912406";//Chave da appi
+    const String _lang = "pt-br";
+    const String _units = "metric";
+
+    const String _apiURL = "api.openweathermap.org";
+    const String _path = "/data/2.5/weather";
+
+    final _parametros = {
+      "q" : _cidadeSelecionada,
+      "appid" : _appid,
+      "lang" : _lang,
+      "units" : _units
+
+    };
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title:Text(_cidadeSelecionada),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            _buildList(_cidades)
+
+
+          ],
+
+        ),
+      ),
+
+    );
+  }
+  Widget _buildList(List <String> listData){
+    return DropdownSearch <String>(
+      mode: Mode.MENU,
+      showSelectedItems: true,
+      // maxHeight: MediaQuery.of(context).size.height - 100,
+      items: _cidades,
+      showSearchBox: true,
+      onChanged: (value) {
+        setState(() {
+          if(value == null || value == ""){
+            _cidadeSelecionada = "Selecione uma cidade";
+
+          } else {
+            _cidadeSelecionada = value;
+          }
+
+        });
+      },
+      validator: (value) => value == null || value.isEmpty
+          ? "Selecione uma cidade"
+          : null,
+      showClearButton: true,
+      clearButton: const Icon(Icons.close),
+      autoValidateMode: AutovalidateMode.onUserInteraction,
+
+
+    );
+
   }
 }
